c07c9cc8add2d580b926de6d086b2f4cfd9555cb
LH <me@lholota.com>
2023-07-18 15:00:49 +0200

    Exit code debug 3

--- nightwatch/commands/signIn.js
+++ nightwatch/commands/signIn.js
@@ -11,8 +11,9 @@ module.exports = {
       .click("input[value='Sign in']")
 
       .element("css selector", "input[type=button][value=No]", result => {
-        console.log("Debug: " + result.status)
-        
+        console.log("Debug")
+        console.log(result.status)
+
         if(result.status != -1) {
           // Sign in succeeded and AAD showed the "Stay signed in?" screen
           browser.click("input[type=button][value=No]")
