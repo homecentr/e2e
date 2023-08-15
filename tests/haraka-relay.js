ca6cec402ae86ae381fcd8f854cbd66f0fb985c7
LH <me@lholota.com>
2023-08-15 12:20:17 +0200

    Tests updated

--- tests/haraka-relay.js
+++ tests/haraka-relay.js
@@ -3,21 +3,30 @@ const nodemailer = require("nodemailer");
 describe('Mailrelay when accessed locally should', async () => {
   this.tags = ["localonly"]
 
-  it('Send e-mail', async () => {
+  // Note: these tests are not running inside of selenium but directly in the runner
+  // which is why the tests bypass dns resolution (the runner does not use Homecentr's custom DNS)
+
+  it('Send e-mail', async (browser) => {
     const transport = nodemailer.createTransport({
-      host: this.globals.smtp_relay_host,
+      host: browser.globals.smtp_relay_host,
       port: 25,
       auth: {
-        user: this.globals.smtp_relay_username,
-        pass: this.globals.smtp_relay_password
+        user: browser.globals.smtp_relay_username,
+        pass: browser.globals.smtp_relay_password
       },
+      tls: {
+        // Server has only domain in certificate and we are using a direct ip to skip dns resolution
+        servername: browser.globals.smtp_relay_servername
+      }
     });
 
-    await transport.sendMail({
-      from: this.globals.smtp_relay_sender,
-      to: this.globals.smtp_relay_recipient,
+    const receipt = await transport.sendMail({
+      from: browser.globals.smtp_relay_sender,
+      to: browser.globals.smtp_relay_recipient,
       subject: "E2E test ✔",
       text: "Hello, world!"
     });
+
+    expect(receipt.accepted.length).to.be.equal(1)
   });
 });
